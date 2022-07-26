dfa8b573b060d8f312829411f8875d5b5b832ead
CamiCifu <camiscifu@gmail.com>
2022-07-26 13:24:17 -0300

    adicion de objeto,array y cosntructor

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//PRACTICA FUNCIONES Y BUCLES
+/*
 alert("Hola, bienvenido a la sección de consultas");
 
 function validarNombre() {
@@ -62,3 +64,65 @@ let emailFormulario = validarEmail(email);
 let motivoFormulario = validarMotivo(motivo);
 let paisFormulario = validarPais(pais);
 let mensajeFormulario = validarMensaje(mensaje);
+*/
+
+//PRACTICA ARRAYS Y OBJETOS
+
+respuestasFormulario = [];
+
+function consultaFormulario() {
+  this.bienvenido = alert("Hola, bienvenido a la sección de consultas");
+  this.validarNombre = (function () {
+    let nombre = prompt("Decime tu nombre");
+    while (nombre === "" || nombre.trim().length === 0) {
+      alert("Por favor re-escribi tu nombre.");
+      let = nombre = prompt("Decime tu nombre");
+    }
+    return nombre;
+  })();
+
+  this.validarEmail = (function () {
+    let email = prompt("Decime tu email");
+    while (email === "" || email.trim().length === 0) {
+      alert("Por favor re-escribi tu email.");
+      let = email = prompt("Decime tu email");
+    }
+    return email;
+  })();
+
+  this.validarMotivo = (function () {
+    let motivo = prompt(
+      "Decime el motivo: \n Suggestion \n Complaint \n Other"
+    );
+    return motivo;
+  })();
+
+  this.validarPais = (function () {
+    let pais = prompt("Decime el pais");
+    while (pais === "" || pais.trim().length === 0) {
+      alert("Por favor decime el pais.");
+      let = pais = prompt("Decime el pais");
+    }
+    return pais;
+  })();
+
+  this.validarMensaje = (function () {
+    let mensaje = prompt("Decime el mensaje");
+    while (mensaje === "" || mensaje.trim().length === 0) {
+      alert("Por favor decime el mensaje.");
+      let = mensaje = prompt("Decime el mensaje");
+    }
+    return mensaje;
+  })();
+}
+
+const consulta1 = new consultaFormulario();
+const consulta2 = new consultaFormulario();
+
+console.log(consulta1);
+console.log(consulta2);
+
+respuestasFormulario.push(consulta1);
+respuestasFormulario.push(consulta2);
+
+console.log(respuestasFormulario);
