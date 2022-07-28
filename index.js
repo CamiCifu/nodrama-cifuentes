70cea06d2db5a93881981ba042aa100c42eff33e
CamiCifu <camiscifu@gmail.com>
2022-07-28 20:57:15 -0300

    agrego console.log a los metodos y agregos this. a los return

--- index.js
+++ index.js
@@ -72,52 +72,62 @@ respuestasFormulario = [];
 
 function consultaFormulario() {
   this.bienvenido = alert("Hola, bienvenido a la sección de consultas");
-  this.validarNombre = (function () {
+  this.validarNombre = function () {
     let nombre = prompt("Decime tu nombre");
     while (nombre === "" || nombre.trim().length === 0) {
       alert("Por favor re-escribi tu nombre.");
       nombre = prompt("Decime tu nombre");
     }
-    return nombre;
-  })();
+    return (this.nombre = nombre);
+  };
 
-  this.validarEmail = (function () {
+  this.validarEmail = function () {
     let email = prompt("Decime tu email");
     while (email === "" || email.trim().length === 0) {
       alert("Por favor re-escribi tu email.");
       email = prompt("Decime tu email");
     }
-    return email;
-  })();
+    return (this.email = email);
+  };
 
-  this.validarMotivo = (function () {
+  this.validarMotivo = function () {
     let motivo = prompt(
       "Decime el motivo: \n Suggestion \n Complaint \n Other"
     );
-    return motivo;
-  })();
+    return (this.motivo = motivo);
+  };
 
-  this.validarPais = (function () {
+  this.validarPais = function () {
     let pais = prompt("Decime el pais");
     while (pais === "" || pais.trim().length === 0) {
       alert("Por favor decime el pais.");
       pais = prompt("Decime el pais");
     }
-    return pais;
-  })();
+    return (this.pais = pais);
+  };
 
-  this.validarMensaje = (function () {
+  this.validarMensaje = function () {
     let mensaje = prompt("Decime el mensaje");
     while (mensaje === "" || mensaje.trim().length === 0) {
       alert("Por favor decime el mensaje.");
       mensaje = prompt("Decime el mensaje");
     }
-    return mensaje;
-  })();
+    return (this.mensaje = mensaje);
+  };
 }
 
 const consulta1 = new consultaFormulario();
+consulta1.validarNombre();
+consulta1.validarEmail();
+consulta1.validarMotivo();
+consulta1.validarPais();
+consulta1.validarMensaje();
 const consulta2 = new consultaFormulario();
+consulta2.validarNombre();
+consulta2.validarEmail();
+consulta2.validarMotivo();
+consulta2.validarPais();
+consulta2.validarMensaje();
 
 console.log(consulta1);
 console.log(consulta2);
@@ -128,7 +138,5 @@ respuestasFormulario.push(consulta2);
 console.log(respuestasFormulario);
 
 //Agrego busqueda
-const consultasOther = respuestasFormulario.find(
-  (m) => m.validarMotivo === "Other"
-);
+const consultasOther = respuestasFormulario.find((m) => m.motivo === "Other");
 console.log(consultasOther);
